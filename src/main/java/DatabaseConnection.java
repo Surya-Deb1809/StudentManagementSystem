e6c6ed337aba1794f9ecb20ac4f4e04aeeb13001
Surya Deb <suryadeb848@gmail.com>
2026-08-19 10:34:19 +0530

    Secure database password handling

--- src/main/java/DatabaseConnection.java
+++ src/main/java/DatabaseConnection.java
@@ -9,7 +9,7 @@ public class DatabaseConnection {
 
     private static final String USER = "root";
 
-    private static final String PASSWORD = "Surya@1234";
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
